83e5af583c25fda035bf3310ef9a5f7b39e59c8b
Xavier Marques <xavierpmarques@gmail.com>
2021-10-28 23:39:23 +0100

    Add .env logic to load MONGDB_URI env var

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.spuaz.mongodb.net/local_library?retryWrites=true&w=majority';
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+// Set up mongoose connection
+var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
